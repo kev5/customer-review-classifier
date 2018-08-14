16316ca1d1790839eafdb741662e153958fa6de2
Keval Khara <kevalkhara95@gmail.com>
2018-08-13 18:21:24 -0700

    svm model

--- svm.py
+++ svm.py
@@ -32,3 +32,28 @@ if not os.path.isfile('./xtr2.npy') or \
     np.save('./xte2', xte2)
     np.save('./ytr2', ytr2)
     np.save('./yte2', yte2)
+
+xtr = np.load('./xtr.npy')
+xte = np.load('./xte.npy')
+ytr = np.load('./ytr.npy')
+yte = np.load('./yte.npy')
+
+xtr2 = np.load('./xtr2.npy')
+xte2 = np.load('./xte2.npy')
+ytr2 = np.load('./ytr2.npy')
+yte2 = np.load('./yte2.npy')
+
+# SVM for classification
+clf = SVC()
+clf.fit(xtr, ytr)
+y_pred = clf.predict(xte)
+m = yte.shape[0]
+n = (yte != y_pred).sum()
+print("Accuracy of Priority = " + format((m-n)/m*100, '.2f') + "%")
+
+clf2 = SVC()
+clf2.fit(xtr2, ytr2)
+y_pred2 = clf2.predict(xte2)
+m = yte2.shape[0]
+n = (yte2 != y_pred2).sum()
+print("Accuracy of Tags = " + format((m-n)/m*100, '.2f') + "%")
