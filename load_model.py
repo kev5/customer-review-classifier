9a7e5bc5d4bed09c37ee7b07ba9c3faaac571585
Keval Khara <kevalkhara95@gmail.com>
2018-08-17 15:22:18 -0700

    prediction

--- load_model.py
+++ load_model.py
@@ -36,3 +36,83 @@ def constructLabeledSentences(data):
     for index, row in data.iteritems():
         sentences.append(LabeledSentence(utils.to_unicode(row).split(), ['Text' + '_%s' % str(index)]))
     return sentences
+
+
+def getEmbeddings(path,vector_dimension=300):
+    """
+    Generate Doc2Vec training and testing data
+    """
+    data = pd.read_csv(path)
+
+    for i in range(len(data)):
+        data.loc[i, 'Message'] = cleanup(data.loc[i,'Message'])
+
+    x = constructLabeledSentences(data['Message'])
+    
+    text_model = Doc2Vec(min_count=1, window=5, vector_size=vector_dimension, sample=1e-4, negative=5, workers=7, epochs=20,
+                         seed=1)
+    text_model.build_vocab(x)
+    train_size = int(0.8 * len(x))
+    test_size = len(x) - train_size
+
+    text_test_arrays = np.zeros((test_size, vector_dimension))
+    test_labels = np.zeros(test_size)
+
+    j = 0
+    for i in range(train_size, train_size + test_size):
+        text_test_arrays[j] = text_model.docvecs['Text_' + str(i)]
+        j = j + 1
+
+    return text_test_arrays
+
+# load the model from disk
+# filename = 'classification/priority.sav'
+# loaded_model = pickle.load(open(filename, 'rb'))
+# result = loaded_model.score("Viasat is awesome", "0")
+# print(result)
+
+# filename = 'classification/tags.sav'
+# loaded_model = pickle.load(open(filename, 'rb'))
+# result = loaded_model.score("Viasat is awesome", "Praise")
+# print(result)
+
+text = 'this router sucks'
+
+s = StringIO(text)
+with open('temp.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Message"])
+    for line in s:
+        f.write(line)
+
+
+#####################if request == "tags":
+filename = 'tags.sav'
+X_test = getEmbeddings("temp.csv")
+loaded_model = pickle.load(open(filename, 'rb'))
+prediction = loaded_model.predict(X_test)
+
+if prediction == 0:
+    print("Inquiry")
+if prediction == 1:
+    print("Account")
+if prediction == 2:
+    print("Service")
+if prediction == 3:
+    print("Praise")
+if prediction == 4:
+    print("DNR")
+if prediction == 5:
+    print("Technical")
+
+
+#####################if request == "priority":
+filename = 'priority.sav'
+X_test = getEmbeddings("temp.csv")
+loaded_model = pickle.load(open(filename, 'rb'))
+prediction = loaded_model.predict(X_test)
+
+if prediction == 0:
+    print("Low")
+if prediction == 1:
+    print("High")
